5163451f07525016aa04a2f84ea81f18d866bbc0
Wesley Ou <69126784+C-FWES@users.noreply.github.com>
2022-06-25 14:29:46 -0400

    handle logout

--- auth.py
+++ auth.py
@@ -2,7 +2,7 @@ from flask import Blueprint, render_template, redirect, url_for, request, flash
 from werkzeug.security import generate_password_hash, check_password_hash
 from models import User
 from app import db
-from flask_login import login_user
+from flask_login import login_user, login_required, logout_user
 
 auth = Blueprint('auth', __name__)
 
@@ -45,5 +45,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    return redirect(url_for('home'))
