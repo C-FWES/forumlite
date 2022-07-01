89559761725b49f32a2b9b315a6a5bd714a3aece
Wesley Ou <69126784+C-FWES@users.noreply.github.com>
2022-07-01 13:06:52 -0400

    view foreign profiles from post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template
 from app import db
 from flask_login import login_required, current_user
+from models import User
 
 main = Blueprint('main', __name__)
 
@@ -9,3 +10,8 @@ main = Blueprint('main', __name__)
 def profile():
     return render_template("profile.html", name=current_user.name, email=current_user.email, time=current_user.last_seen)
 
+@main.route('/profile/<int:id>')
+@login_required
+def view_profile(id):
+    user = User.query.get_or_404(id)
+    return render_template("profile.html", name=user.name, email=user.email, time=user.last_seen)
